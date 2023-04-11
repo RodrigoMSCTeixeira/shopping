0a2175581aabda765cc9f19efd4dd438717c1207
Rodrigo Teixeira <rteixerira@gmail.com>
2023-04-11 19:15:48 -0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,49 @@
 <template>
-  <nav>
-    <router-link to="/">Início</router-link>
-    -
-    <router-link to="/basket">
-      Carrinho ({{ productsInBag.length }})
-    </router-link>
-  </nav>
+  <div class="menu">
+    <Logo />
+    <nav>
+      <router-link to="/">Início</router-link>
+      -
+      <router-link to="/basket">
+        Carrinho ({{ productsInBag.length }})
+      </router-link>
+    </nav>
+  </div>
   <router-view />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { mapState, useStore } from 'vuex'
+import { defineComponent } from "vue";
+import { mapState, useStore } from "vuex";
+import Logo from "@/components/Logo/index.vue";
 
 export default defineComponent({
   data: () => {
     return {
       store: useStore(),
-    }
-  },
-  created() {
-    this.store.dispatch('loadProducts')
-    this.store.dispatch('loadBag')
+    };
   },
 
-  computed: mapState(['productsInBag']),
-})
+  components: {
+    Logo,
+  },
+
+  created() {
+    this.store.dispatch("loadProducts");
+    this.store.dispatch("loadBag");
+  },
+
+  computed: mapState(["productsInBag"]),
+});
 </script>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -37,15 +52,24 @@ export default defineComponent({
   color: #2c3e50;
 }
 
+.menu {
+  background: #2c3e50;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 nav {
+  margin: 0 30px;
   padding: 30px;
+  color: #fff;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #fff;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #ff3;
     }
   }
 }
